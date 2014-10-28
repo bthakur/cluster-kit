17916295024ba8fa592d755e163c45666cfaf8c1
Bhupender Thakur <bthakur@dhcp153-19.nersc.gov>
2014-10-28 11:04:59 -0700

    more sge changes

--- sge/parse_sgeacct.py
+++ sge/parse_sgeacct.py
@@ -3,7 +3,7 @@
 import os, sys
 import re
 import getopt
-import gzip
+import gzip, bz2
 
 helptext="""
 +-------+
@@ -13,15 +13,13 @@ helptext="""
 ./parse_sgeacct.py  -b Begin_date: same as qacct
                     -e End_date: same as qacct
                     -j Use the jobid
-
-  # This is what you would really use it for
+                    -L last job
+                    -Q Pass known options to qacct
                     -a At_this_time(Begin and End date must lie within)
-
-  # Use accounting files(comma separated, or zipped)
-                    -f  accounting-20140405,accounting-20140505.gz 
-
-  ./parse_sgeacct.py -h  Will print help message and exit
-  ./parse_sgeacct.py     Without arguments, shoud process default accounting
+                    -f accounting-20140405,accounting-20140505.gz 
+                       # Use accounting files(comma separated, or zipped)
+./parse_sgeacct.py  -h # Will print help message and exit
+./parse_sgeacct.py     # No arguments, shoud process default use qacct?
 
 """
 
@@ -29,115 +27,187 @@ def help():
     print helptext
 
 #----------------------------
-# SGE stuff:
+# SGE support:
 #----------------------------
 def check_sge():
     global default_acct, f_sge
     try:
-      dir_sge_root=os.environ["SGE_ROOT"]
-      dir_sge_cell=os.environ["SGE_CELL"]
-      f_sge=True
+        dir_sge_root=os.environ["SGE_ROOT"]
+        dir_sge_cell=os.environ["SGE_CELL"]
+        f_sge=True
     except KeyError:
-      print "SGE_ROOT/CELL undefined, Will try reading files locally"
-      f_sge=False
+        print "SGE_ROOT/CELL undefined, Will try reading files locally"
+        f_sge=False
 
-    if f_sge:				# With sge		
-      dir_cur_acct='common'		# Def. location for accounting
-      dir_log_acct='log'		# Def. location for log
-      sge_cur_acct='accounting'		# Def. accounting file
-      sge_cur_rept='reporting'		# Def. reporting file
-      #
-      sge_rootcell=dir_sge_root+'/'+dir_sge_cell
-      default_acct=sge_rootcell+'/'+dir_cur_acct+'/'+sge_cur_acct
-      default_rept=sge_rootcell+'/'+dir_cur_acct+'/'+sge_cur_rept
-      #	
+    if  f_sge:				# With sge		
+        dir_cur_acct='common'		# Def. location for accounting
+        dir_log_acct='log'		# Def. location for log
+        sge_cur_acct='accounting'		# Def. accounting file
+        sge_cur_rept='reporting'		# Def. reporting file
+        #
+        sge_rootcell=dir_sge_root+'/'+dir_sge_cell
+        default_acct=sge_rootcell+'/'+dir_cur_acct+'/'+sge_cur_acct
+        default_rept=sge_rootcell+'/'+dir_cur_acct+'/'+sge_cur_rept
+        #	
     else:				# Without sge
-      dir_cur_acct=os.getcwd()          # Def. location is cwd
-      dir_log_acct=os.getcwd()          # Def. location for cwd
-      default_acct='accounting'
-      default_rept='reporting'
+        dir_cur_acct=os.getcwd()          # Def. location is cwd
+        dir_log_acct=os.getcwd()          # Def. location for cwd
+        default_acct=dir_cur_acct+'/'+'accounting'
+        default_rept=dir_cur_acct+'/'+'reporting'
 
   #Change if your defaut is somewhere else
   #default_acct=?some_usr_global_copy
     print default_acct
 
 #----------------------------
-# Files Check: Parse files
-#----------------------------
-#----------------------------
 # Parse Input:  getopt
 #----------------------------
 
 def check_opts():
+#-# Globals at the top
     global f_fopt;global f_aopt
     f_sge=False; f_fopt=False; f_aopt=False;
-    print default_acct;
+    #print default_acct;
+#-# Options we can parse
     opts='-f:-a:-h'
-    if sys.argv:
-      args=sys.argv[1:]
+    if  sys.argv:
+        args=sys.argv[1:]
+        optlist,arglist=getopt.getopt(args,opts)
+        print "+-----------------+"
+        print "| Options Supplied|"
+        print "+-----------------+"
+        print " %s" %optlist
+        opts_dic={}
+        for o,a in optlist:
+            if o=='-f':
+                f_fopt=True
+                f_args=a
+            elif o=='-a':
+                f_aopt=True
+                #print a
+            elif o=='-h':
+                help()
+                sys.exit()
 
-    optlist,arglist=getopt.getopt(args,opts)
-    print "Options Supplied \n %s" %optlist
-
-    opts_dic={}
-
-    for o,a in optlist:
-      if o=='-f':
-        f_fopt=True
-        list_of_files=files_to_list(a)
-      elif o=='-a':
-        f_aopt=True
-        print a
-      elif o=='-h':
-        help()
-        sys.exit()
-
-  #def files_to_list(f):
+#-# Read accounting files(default and passed with -f)
+  #----------------------------
+  # File Checks: Parse files
+  #----------------------------
     g=[]
-    g+=[default_acct]
-    #print type(g),default_acct,g
-    if f_fopt:
-      mf=f.split(',')
-      # Check if files exist
-      for f in mf:
-        if os.path.isfile(f) or \
-           os.path.isfile(str(sge_rootcell+'/'+dir_cur_acct+'/'+f)) or \
-           os.path.isfile(str(sge_rootcell+'/'+dir_log_acct+'/'+f)) :
-             print "is  file",f
-             g.append(f)
-        else:
-             print "not file",f
+    h=[]
+    acct_jobs=[]
+    acct_file_list=[]
+    if os.path.isfile(default_acct):
+#----# Add default accounting
+       g+=[default_acct]
+       acct_file_list.append((default_acct,'def'))
+       #print " Adding default accounting", (default_acct,'def')
+#----# Check if other files provided exist
+       if  f_fopt:
+           mf=f_args.split(',')
+           #print 'f_sge',f_sge
+#--------# Check SGE default directories
+           if  f_sge:
+               for f in mf:
+                 for fullf in  \
+                     sge_rootcell+'/'+dir_cur_acct+'/',+f, \
+                     sge_rootcell+'/'+dir_log_acct+'/'+f:
+                     if os.path.isfile(fullf):
+                        g.append(fullf)
+#--------# Check cwd(default) directory
+           else:
+               for f in mf:
+                 for fullf in [os.getcwd()+'/'+str(f)] :
+                     #print fullf
+                     if os.path.isfile(fullf):
+                        g.append(fullf)
+#-# Reduce list to remove duplicates
     g=list(set(g))
-    print type(g),g
-    #sys.exit()
-    #if len(g)==0:
-    #    print "None of the files could be used"
-    #    # Use default accounting file
-    #    g.append(default_acct)
-    #else:
-    #    # Check if the files need to be unzipped
-    #    for f in g:
-    #        if f.endswith('.gz'):
-    #            print "A  zipped file", f
-    #if len(g) > 1:
-    #    print "Several files need concatenation"
-    #    sys.exit() 
-
+    #print "    Full list",g
+#-# Check if the files need to be unzipped
+    for f_i in g:
+        #print "----+----------------+ "
+        #print "    | Parsing file : %s " %f_i
+        #print "    +----------------+ "
+        #print "    %s" %f_i
+        f=str(f_i)
+        #continue
+        if f.endswith('.tar.gz'):
+             fname=f[:-7]
+             #comm=['gunzip',fname]   
+             #print "A  zipped file", f
+             # Read contents of gz file
+             break
+        elif f.endswith('.tar.bz2'):
+             fname=f[:-8]
+             break
+        elif f.endswith('.tgz'):
+             fname=f[:-4]
+             break
+        elif f.endswith('.tar.Z'):
+             fname=f[:-6]
+             break
+        elif f.endswith('.gz'):
+             fname=f[:-3]
+             #print 'fname',fname
+             #print 'f_i',f_i
+             try:
+                 with gzip.open(f_i,'rb') as fi:
+                      lines=fi.readlines()
+                      acct_file_list.append((fname,'gz'))
+                      acct_jobs.extend(lines)
+                      print "    (%s, %s)" %(fname,'gz')
+                      #print acct_jobs[0]
+                      #sys.exit()
+             except:
+                 print "Error reading file", (fname,'gz')
+                 pass
+        elif f.endswith('.Z'):
+             fname=f[:-2]
+             #comm=['zcat',fname]
+             break
+        elif f.endswith('.bz2'):
+             fname=f[:-3]
+             try:
+                 h=b2.decompress(f_i)
+                 with open(h,'r') as fi:
+                      acct_file_list.append((fname,'bz2'))
+                      acct_jobs.append(fi.readlines())
+                      print "    (%s, %s)" %(fname,'bz2')
+                      #print acct_jobs[0]
+             except:
+                 print "Error reading", (fname,'bz2')
+                 #comm=['bzip2',f]
+             break
+        else:
+             fname=f_i
+             acct_file_list.append((fname,'nozip'))
+             #print "    No extension, will try reading as such", (fname,'nozip')
+             try:
+                 with open(f_i,'r') as fi:
+                      lines=fi.readlines()
+                      acct_file_list.extend((fname,'nozip'))
+                      acct_jobs.extend(lines)
+                      print "    (%s, %s)" %(fname,'nozip')
+                      #print acct_jobs[0]
+             except Exception as err:
+                 print err
+                 #pass
 
 #----------------------------
-# Parse Files: add files
+# Parse option -Q: pass this assembled file/other options to qacct
 #----------------------------
 
     #print list_of_files
-    a=[]
+    #a=[]
     #if a:
-    for f in g:
-        with open(f,'read') as fi:
-            a.extend(fi.readlines())
+    #for f in g:
+    #    with open(f,'read') as fi:
+    #        a.extend(fi.readlines())
     
-    print a[0]
-    print a[1]
-    print a[-1]
+    #print a[0]
+    #print a[1]
+    #print a[-1]
 
 #----------------------------
 # Main
