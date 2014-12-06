2f73d0f3902d5a75570e9bff9ec4aca25e357fea
Bhupender Thakur <bthakur@DOE6967036.local>
2014-12-05 21:51:44 -0800

    slight change

--- common/parse_qstat.py
+++ common/parse_qstat.py
@@ -7,6 +7,17 @@ import subprocess
 global schedulers
 schedulers=['uge', 'torque', 'slurm']
 
+def get_version(com):
+    try:
+        p=subprocess.Popen(com,stdout=subprocess.PIPE, \
+                          stderr=subprocess.PIPE)
+    except:
+        print "    %s command failed" %com_qst
+    out,err=p.communicate()
+    if err != '':
+        ver=out[2]
+    
+
 def check_scheduler():
     obj_sch={}
     com_qst=['which','qstat']
@@ -22,8 +33,6 @@ def check_scheduler():
     for sch in schedulers:
       if sch in whi_qst_out:
         obj_sch['name']=sch
-        obj_sch['qstat']=qstat
-        obj_sch['qhost']=qstat
         obj_sch['env']={'PATH':whi_qst_out[:-len('/qstat')-1]}
       else:
         obj_sch['name']=None
@@ -31,34 +40,40 @@ def check_scheduler():
         if sch == 'uge':
           obj_sch['env']['SGE_ROOT']=''
           obj_sch['env']['SGE_CELL']=''
-          obj_sch['sch_stat']=['qstat','-u','*','-v']
-          obj_sch['sch_host']=['qhost']
+          obj_sch['sch_stat']=['qstat','-u','*']
+          obj_sch['sch_host']=['qhost','-j']
+          obj_sch['get_ver']=['-help']
           print "Supports %s" %sch
         elif sch == 'torque':
           obj_sch['env']['PBS_HOME']=''
           obj_sch['env']['PBS_SERVER']=''
-          obj_sch['sch_stat']=qstat
-          obj_sch['sch_host']=pbsnodes
+          obj_sch['sch_stat']=['qstat']
+          obj_sch['sch_host']=['pbsnodes']
+          obj_sch['get_ver']=['-v']
           print "Supports %s" %sch
         elif sch == 'slurm':
           obj_sch['env']['SLURM_']=''
           obj_sch['env']['SLURM_']=''
           obj_sch['sch_stat']=['sinfo']
           obj_sch['sch_host']=['scontrol','show','node']
+          obj_sch['get_ver']=['-v']
           print "Supports %s" %sch
         else:
           print "Found no supported scheduler...Exiting"
-          #sys.exit()
+          sys.exit()
+        obj_sch['sch_ver']=get_version[obj_sch['sch_stat'],obj_sch['get_ver']]
       else:
         obj_sch['name']=None
+      
     print '''
-    Checking scheduler support %s
+    Checking scheduler support %s %s
     Command tried              %s
     Return Error               %s
     Found support for          %s
-    ''' %(schedulers, obj_sch['qstat'],obj_sch['err_code'],obj_sch['name'])
+    ''' %(schedulers, obj_sch['qstat'],obj_sch['sch_ver'],obj_sch['err_code'],obj_sch['name'])
     
     return obj_sch
+    sys.exit()
 
 def run_command(com_inp):
     com_out={}
