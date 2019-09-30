489296d28c0771d365bfae3a0c069492addfa622
thakurb1 <bhupender.thakur@roche.com>
2019-09-30 09:09:35 -0700

    Uodate to master

--- sge/parse_sgeacctfile.py
+++ sge/parse_sgeacctfile.py
@@ -3,7 +3,13 @@
 #Bhupender Thakur 2019
 
 import argparse as ap
+import datetime as dt
 import sys
+
+def epoch_to_date(secs_i):
+    t_date=dt.datetime.fromtimestamp(
+           secs_i.strftime('%Y-%m-%d:%H%M'))
+    return t_date
 
 parser = ap.ArgumentParser()
 
@@ -19,8 +25,23 @@ if args.file_inp:
 else:
     parser.print_help()
     sys.exit()
+
 OneK=1000.
 OneM=1000000.
+
+#print dt.datetime.tzinfo.est()
+t_now=dt.datetime.today()
+t_epoch=dt.datetime(1970,1,1)
+t_beg2019=dt.datetime(2019,01,01,0,0)
+#
+t_nowsecs=t_now
+t_del2019secs=(t_beg2019 - t_epoch).total_seconds()
+t_del2019days=(t_beg2019 - t_epoch).days
+print t_epoch, t_beg2019,t_now
+print t_del2019secs,t_del2019days
+
+#sys.exit()
+
 for l in lines:
     line=l.split(':')
     if l[0] != '#':
@@ -28,9 +49,15 @@ for l in lines:
         host=line[1]
         user=line[3]
         jobid=line[5]
-        tsub=int(line[8]/OneM); tbeg=int(line[9]/OneM); tend=int(line[10]/OneM)
+        #slots=
+        #vmem
+        tsub=float(line[8])/OneK;  tsub_19=tsub-t_del2019secs
+        tbeg=float(line[9])/OneK;  tbeg_19=(tbeg-t_del2019secs)
+        tend=float(line[10])/OneK; tend_19=(tend-t_del2019secs)
+        twai=round(tbeg_19-tsub_19,2)
+        trun=round(tend_19-tbeg_19,2)
         #print l
-        print queue,host,user,jobid,tsub,tbeg,tend
-
+        #print queue,host,user,jobid,tsub_19,tbeg_19,tend_19
+        print queue,host,user,jobid,twai,trun
 
 
